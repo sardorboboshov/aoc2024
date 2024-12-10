fd13efb65669ba6d6800bc20239edeb42bb34a5a
sardorboboshov <89270394+sardorboboshov@users.noreply.github.com>
2024-12-10 10:35:43 +0500

    Create day10.2.py

---
+++ day10.2.py
@@ -0,0 +1,47 @@
+from collections import defaultdict, deque, Counter
+import bisect
+from functools import cache
+from math import inf
+rint = lambda: int(input())
+def rlist(n=None):
+    if n is None:
+        return list(map(int, input().split()))
+    return [list(map(int, input().split(''))) for _ in range(n)]
+  
+n = 8
+n = 45
+
+arr = [['' for i in range(n)] for j in range(n)]
+zeros = []
+for i in range(n):
+  x = list(map(int, input()))
+  for idx, num in enumerate(x):
+    arr[i][idx] = num
+    if num == 0:
+      zeros.append((i, idx))
+
+dirs = ((0,1), (0, -1), (1,0),(-1,0))
+
+def dfs(i, j, sh):
+  if not (0 <= i < n and 0 <= j < n):
+    return 0
+  
+  if arr[i][j] != sh:
+    return 0
+  
+  if arr[i][j] == 9:
+    return 1
+  ans = 0
+  for di,dj in dirs:
+    ni,nj = i+di,j+dj
+  
+    if 0 <= ni < n and 0 <= nj < n and arr[ni][nj] == sh + 1:
+      ans += dfs(ni,nj,sh+1)
+  return ans
+
+ans = 0
+for i,j in zeros:
+  # print(dfs(i,j,0))
+  ans += dfs(i,j,0)
+  
+print(ans)
